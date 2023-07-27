8bd34fe612bdb20dca36305bc266cc952bfd44ed
Sayantan Das <sdas45758@gmail.com>
2023-07-27 09:42:12 +0530

    Commiting FizzBuzz.java

---
+++ Java/Java Basics/src/com/sayantandas/FizzBuzz.java
@@ -0,0 +1,20 @@
+package com.sayantandas;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        if(number % 15 == 0)    {
+            System.out.println("FizzBuzz");
+        }
+        else if(number % 5 == 0)   {
+            System.out.println("Fizz");
+        }
+        else if(number % 3 == 0)  {
+            System.out.println("Buzz");
+        }
+    }
+}
