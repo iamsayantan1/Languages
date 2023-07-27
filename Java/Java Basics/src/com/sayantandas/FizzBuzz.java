e1dcb86a5bb3b11137110ff80b26fbb325afaeb8
Sayantan Das <sdas45758@gmail.com>
2023-07-27 23:28:37 +0530

    FizzBuzz Game is updated, Now user can play FizzBuzz without rerunning programming every time.

--- Java/Java Basics/src/com/sayantandas/FizzBuzz.java
+++ Java/Java Basics/src/com/sayantandas/FizzBuzz.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = sc.nextInt();
-        if(number % 15 == 0)    {
-            System.out.println("FizzBuzz");
+        int number;
+        System.out.print("Press 1 to start FizzBuzz Game else press any number to end: ");
+        number = sc.nextInt();
+        if(number == 1) {
+            while (!(number == 404)) {
+                System.out.print("Number: ");
+                number = sc.nextInt();
+                if (number % 15 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (number % 5 == 0) {
+                    System.out.println("Fizz");
+                } else if (number % 3 == 0) {
+                    System.out.println("Buzz");
+                }
+            }
         }
-        else if(number % 5 == 0)   {
-            System.out.println("Fizz");
-        }
-        else if(number % 3 == 0)  {
-            System.out.println("Buzz");
-        }
+        System.out.println("Game Over...");
     }
 }
