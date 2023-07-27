49bd5275825584b31a920cc10611c251fa4f1038
Sayantan Das <sdas45758@gmail.com>
2023-07-28 01:08:51 +0530

    MortgageCalculator is updated, Now it validates the user input every time.

--- Java/Java Basics/src/com/sayantandas/MortgageCalculator.java
+++ Java/Java Basics/src/com/sayantandas/MortgageCalculator.java
@@ -8,20 +8,50 @@ public class MortgageCalculator {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal;
+        float annualInterest;
+        float monthlyInterest;
+        byte noOfYears;
+        int noOfPayments;
+        double mortgage;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print("Principal (₹1K - ₹10L): ");
+            principal = scanner.nextInt();
+            if ( principal >= 1_000 && principal <= 10_00_000 )  {
+                break;
+            }
+            else {
+                System.out.println("Enter a number between 1,000₹ and 10,00,000₹.");
+            }
+        }
 
-        System.out.print("Annual Interest Rate in %: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        while (true) {
+            System.out.print("Annual Interest Rate in %: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                break;
+            }
+            else {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+            }
+        }
 
-        System.out.print("Period (Years): ");
-        byte noOfYears = scanner.nextByte();
-        int noOfPayments = noOfYears * MONTHS_IN_YEAR;
+        monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
 
-        double mortgage = principal
+        while (true) {
+            System.out.print("Period (Years): ");
+            noOfYears = scanner.nextByte();
+            if (noOfYears > 0 && noOfYears <= 30)   {
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
+
+        noOfPayments = noOfYears * MONTHS_IN_YEAR;
+
+        mortgage = principal
                 * (monthlyInterest * Math.pow((1 + monthlyInterest),noOfPayments))
                 / (Math.pow((1 + monthlyInterest),noOfPayments) - 1);
 
